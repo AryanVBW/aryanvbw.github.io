9101f20a58965c480b6bec7f73c8e0e0662c7d74
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2024-01-18 17:08:13 +0530

    Add files via upload

---
+++ wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -0,0 +1,13 @@
+/*! elementor - v3.19.0 - 17-01-2024 */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*******************************************************!*\
+  !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
+  \*******************************************************/
+
+
+window.elementor.start();
+/******/ })()
+;
+//# sourceMappingURL=editor-loader-v1.js.map
